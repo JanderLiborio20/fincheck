6fa2e8cf358f5e24cc7a1f9ce5c65c4b61f8bad5
jander.liborio <jandernunes14@gmail.com>
2023-12-04 11:11:34 -0400

    finish useMenu

--- frontend/src/view/components/DropdownMenu/DropdownMenuItem.tsx
+++ frontend/src/view/components/DropdownMenu/DropdownMenuItem.tsx
@@ -4,16 +4,19 @@ import { cn } from '../../../app/utils/cn';
 interface DropdownMenuItemProps {
   children: React.ReactNode;
   className?: string;
+  onSelect?(): void;
 }
 
 export function DropdownMenuItem({
   children,
   className,
+  onSelect,
 }: DropdownMenuItemProps) {
   return (
     <RdxDropdownMenu.Item
+      onSelect={onSelect}
       className={cn(
-        `outline-none min-h-[48px] flex items-center p-4 text-sm text-gray-800 hover:bg-gray-50 rounded-2xl transition-colors`,
+        `outline-none min-h-[48px] flex items-center p-4 text-sm text-gray-800 data-[highlighted]:bg-gray-50 rounded-2xl cursor-pointer transition-colors `,
         className,
       )}
     >
